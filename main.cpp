49a41ae9fab353aaadbb104275e14dbc30e0c7f8
hosternus <90009071+hosternus@users.noreply.github.com>
2024-10-10 04:41:41 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+long process(long arr[], int n, long misha=0, long petya=0, int used_chachki=0)
+{
+    if (used_chachki == n) { return abs(misha - petya); }
+
+    if (misha <= petya) { misha += arr[used_chachki]; }
+    else { petya += arr[used_chachki]; }
+    
+    return process(arr, n, misha=misha, petya=petya, used_chachki=used_chachki+1);
+}
+
+int main(void)
+{
+    // INPUT N (кол-во чашек)
+    short n = 0;
+    cin >> n;
+
+    // INPUT ARR(N) (грамовка чашек)
+    long chachki[n];
+    for (short i = 0; i < n; i++) { cin >> chachki[i]; }
+
+    // SORTING (грамовку)
+    long chachki_i, chachki_j;
+    for (short i = 0; i < n; i++)
+    {
+        for (short j = i; j < n; j++)
+        {
+            if (chachki[i] < chachki[j])
+            {
+
+                chachki_i = chachki[i];
+                chachki_j = chachki[j];
+                chachki[i] = chachki_j;
+                chachki[j] = chachki_i;
+            }
+        }
+    }
+
+    // ВЫЧИСЛЕНИЯ И ВЫВОД
+
+    cout << process(chachki, n) << endl;
+
+    return 0;
+}
---
+++ prototip.py
@@ -0,0 +1,96 @@
+# n = int(input())
+# gramovki = list(map(int, input().split()))
+
+
+
+def sum(arr: list) -> int:
+    sm = 0
+    for i in arr:
+        sm += i
+    return sm
+
+def sort(arr: list) -> list:
+    long = len(arr)
+    for i in range(long):
+        for j in range(i, long):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+print(sort([9, 8, 7, 6, 4, 3, 9]))
+
+
+def f(free, misha=0, petya=0):
+
+    free = sort(free)
+
+    if sum(free) == 0:
+        return abs(misha - petya)
+
+    if misha <= petya:
+        misha += free[0]
+        return f(free[1:], misha, petya)
+    
+    if misha >= petya:
+        petya += free[0]
+        return f(free[1:], misha, petya)
+        
+
+
+print(
+    f([
+        1, 2, 3, 4, 5, 6, 7, 8, 9
+    ])
+)
+
+
+
+print(
+    f([
+        5, 7, 8
+    ])
+)
+
+print(
+    f([
+        4, 4, 5, 6, 7, 3, 9
+    ])
+)
+
+
+# 1 4 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def f(free, misha=[], petya=[]):
+#     if len(free) == 0:
+#         return abs(misha - petya)
+    
+#     if misha <= petya and len(free) != 0:
+#         misha += (
+#             free.pop(free.index(min(free)))
+#         )
+#         return f(free, misha, petya)
+#     if misha >= petya and len(free) != 0:
+#         petya += (
+#             free.pop(free.index(max(free)))
+#         )
+#         return f(free, misha, petya)
+#     if misha == petya:
+#         return 0
