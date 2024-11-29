901f174dd3a7d18208d8104ce48d22f3b46415d2
hosternus <90009071+hosternus@users.noreply.github.com>
2024-11-29 17:47:19 +0300

    done

--- minchars.cpp
+++ minchars.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 
-// 3
-
 int main(int argc, char** argv) {
-    while (argc < (2 << std::atoi(argv[1])) ) {std::cout << (argc <<= 1) << std::endl;}
+    while (argc < (2 << std::atoi(argv[1])) )if (std::cout << (argc <<= 1) << std::endl) {}
 }
